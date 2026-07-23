b288c24053c5096824ebefab3a4c56a4eefeb3f4
Caique Dadonas <69750914+Sikynat@users.noreply.github.com>
2026-07-23 00:04:46 -0300

    Add: Class Product.java

--- src/com/sindesi/estoque/Main.java
+++ src/com/sindesi/estoque/Main.java
@@ -1,8 +1,30 @@
 package com.sindesi.estoque;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Olá, mundo!");
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String valor;
+        String nome;
+        int idade;
+        float nota;
+
+        System.out.print("Digite seu nome: ");
+        nome = teclado.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        idade = teclado.nextInt();
+
+        valor = Integer.toString(idade);
+
+        System.out.print("Digite sua nota: ");
+        nota = teclado.nextFloat();
+
+        System.out.printf("Olá, meu nome é %s e eu tenho %s anos de idade.\nMinha nota na prova foi: %.2f", nome, idade, nota);
+
+
+
     }
 }
 
---
+++ src/com/sindesi/estoque/Product.java
@@ -0,0 +1,16 @@
+package com.sindesi.estoque;
+
+public class Product {
+        private String description;
+        private int quantity;
+        private float value;
+
+        Product(String description, int quantity, float value){
+            this.description = description;
+            this.quantity = quantity;
+            this.value = value;
+        }
+    }
+
+
+
